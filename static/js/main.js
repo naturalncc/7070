73cb4b1726c5ef3bad11e179c8afcff9b85c9b45
naturalncc <nmedic7070@gmail.com>
2025-08-13 09:26:16 -0400

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -289,7 +289,7 @@ class HolisticApp {
         chatInput.value = '';
         this.showTypingIndicator();
         try {
-            const response = await fetch('/netlify/functions/chat', {
+            const response = await fetch('/.netlify/functions/chat', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ message })
